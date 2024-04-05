428201996d895bbeadfe7397b259590d7ff7acbd
rayane-bouchair <bouchairrayane01@gmail.com>
2024-04-05 15:32:05 +0100

    delete unused file

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-#TODO: afficher nbr regle  appliquee
-                # infere = CTkLabel(master=scrollable_frame2,text=f"==>(appliquer R{reglesCopy.index(regle) + 1})",font=("Helvetica", 15,"bold"))
-                # infere.pack_configure(padx=12, pady=12,anchor="center")
-                
